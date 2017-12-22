aea55202be20ad7008f6c6ef83802bff0e21544f
Nasty Gloss <notabeech@gmail.com>
2017-12-22 23:54:21 +0300

    Button UP

---
+++ dist/js/parallax.js
@@ -0,0 +1,13 @@
+$(window).scroll(function() {
+	parallax();
+})
+
+function parallax() {
+
+	var wScroll = $(window).scrollTop();
+	var quoteScroll = document.querySelector(".quote").getBoundingClientRect().top + wScroll;
+
+		$(".header").css("background-position", "center -" + wScroll/5 + "%");
+
+	$(".quote").css("background-position", "center " + (wScroll + document.documentElement.clientHeight - quoteScroll)/12 + "%");
+}
--- gulpfile.js
+++ gulpfile.js
@@ -21,11 +21,10 @@ gulp.task("less", function() {
 	 .pipe(browserSync.reload({stream: true}));
 });
 
-
-
 gulp.task("scripts", function() {
 	return gulp.src([
-			//подключаем минифицированные файлы библиотек из dist
+			"app/libs/jquery/dist/jquery.min.js",
+			"app/libs/magnific-popup/dist/jquery.magnific-popup.min.js"
 		])
 	.pipe(concat("libs.min.js"))
 	.pipe(uglify())
